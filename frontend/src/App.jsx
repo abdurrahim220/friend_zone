9a00fdd2d34efc91bd8a017d390a70b230e77faf
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-02-16 23:36:56 +0600

    side bard done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/Signup/SignUp";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       {/* <Login /> */}
 
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Home />
     </div>
   );
 }
