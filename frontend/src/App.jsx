a636fb460fc9e576788d249af083e5f1c77bc9b7
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-02-16 22:33:38 +0600

    login and sing up component done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/Login/Login";
+import SignUp from "./pages/Signup/SignUp";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      {/* <Login /> */}
+
+      <SignUp/>
     </div>
   );
 }
